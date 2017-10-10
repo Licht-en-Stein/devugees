9095793925dc365c2f18ec4486c6c7c106034163
Jan Schulz <jan.schulz@cileria.com>
2017-10-10 15:06:13 +0200

    removed solutions :-)

--- jquery/traversing-task-1/index.js
+++ jquery/traversing-task-1/index.js
@@ -1,11 +1,3 @@
 $(document).ready(function () {
-  $('body > div > div > div')
-    .first()
-    .children().first()
-    .text('1');
 
-  $('body > div')
-    // .first()
-    .next()
-    .css('background-color', 'red')
 });
--- jquery/traversing-task-2/index.js
+++ jquery/traversing-task-2/index.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-  $("div > fieldset").first().find("input").prop("checked", true);
-  //$("input").first().prop("checked", true);
 
-  $('div > fieldset').eq(1).find("legend").text("MC Hamer");
-  $("body > div > div").first().find("em > span").text("italic");
-  $('body > div > div > div').find('p').eq(2).css({'background-color':  'red'});
-  // $('p').eq(2).css({'background-color':  'red'});
-  $("ul > li > ul > li > ul > li").eq(2).text("thing");
-  $("span").last().text("!");
 });
 
