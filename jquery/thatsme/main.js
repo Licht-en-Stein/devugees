d8a5a5fca2cf1d779379a1dbfecf9697c7930513
foobaroo <jan.schulz@cileria.com>
2017-11-05 21:35:04 +0100

    added thatsme and sql

--- jquery/thatsme/main.js
+++ jquery/thatsme/main.js
@@ -1,8 +1,25 @@
 $(document).ready( function() {
 	console.log('Document is loaded.');
 
+	$('body').fadeIn('slow');
 
+	$('a#nav-about-me').on('click', function() {
+		showPage('about-me');
+	});
 
-	
+    $('a#nav-fav-city').on('click', function() {
+        showPage('my-favourite-city');
+    });
+
+    $('a#nav-fav-songs').on('click', function() {
+        showPage('my-favourite-songs');
+    });
+
+	function showPage( page ) {
+		var x = 'div#content > div:not(#' + page + ')';
+		$(x).hide();
+		$('#'+page).fadeIn('slow');
+	}
+
 });
 
