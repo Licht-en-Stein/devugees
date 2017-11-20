be1f1d5872f0481a27c8e94927a193fcb75ea4ab
Jan Schulz <jan.schulz@devugees.org>
2017-11-20 07:37:30 +0100

    added ajax and dommanipulation

---
+++ jquery/ajax/main.js
@@ -0,0 +1,46 @@
+$(document).ready( function() {
+    console.log('Document is loaded.');
+
+
+    // 1. use the $.ajax method, to load 
+    //    the data.json and load its contents
+    //    into the table #tbl1 
+
+    $('.getdatasuccess').click(function() {
+
+    });
+
+    // 2. create an ajax requests that 
+    //    loads a file that DOES NOT exist
+    //    and print out the status code in the "error" function
+    //    of the ajax object.
+
+    $('.getdatafail').click(function() {
+
+    });
+
+    // 3. create an $.get request that loads
+    //    the "data.html" file into the #htmldata
+    //    div
+
+    $('.getdata').click(function() {
+
+    });
+
+    // 4. load the script "data.js" using the
+    //    $.getScript function
+
+    $('.getscript').click(function() {
+
+    });
+
+    // Same as in 1., load the data.txt again using the $.getJSON function 
+    // and load the contents into the table #tbl2
+
+    $('.getjson').click(function() {
+
+    });
+
+
+});
+
---
+++ jquery/dommanipulation/main.js
@@ -0,0 +1,49 @@
+$(document).ready( function() {
+	console.log('Document is loaded.');
+
+    // html() and text()
+	// task: when the user clicks on Update HTML,
+	//       the div with the class "text" will be
+	//       updated with the html() function
+	//
+	//       when the user clicks on Update Text,
+	//       it will be updated with text() function
+	//
+	//       What is a good way to test your code?
+
+
+    // attr() and removeAttr()
+    //
+	// task: a) when the user clicks on the Add-button,
+	// 	     the id of the textbox "text-2" will be
+	//       changed to "text-2b".
+	//
+	//       b) when the user clicks on remove, the
+	//          style of "text-2b" will be removed entirely
+
+
+	// Adding DOM-Elements
+	//
+	// task: a) when the user clicks on "Insert at end",
+	//          a new fruit is added at the end of the
+	//			list
+	//       b) when the user clicks on "Insert at end",
+	//			a new fruit is added at the beginning of the list
+	//       c) when the user clicks on "first becomes last"
+	//          the first element will be added to the end of the list
+
+
+	// Removing and Detaching Elements
+	//
+	// task: a) when the user clicks on the button remove,
+	//		    the div with class box will be removed
+	//       b) when the user clicks on the button detach,
+	//			the div with class box will be detached
+    //       c) when the user clicks on the button empty,
+    //			the div with class box will be emptied
+    //       d) when the user clicks on the button add back,
+    //			the div with class box will be reattached
+
+
+});
+
