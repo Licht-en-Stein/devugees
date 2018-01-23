4063843b2b8b895dda51f26a5db66beef15164e7
Jan Schulz <jan.schulz@devugees.org>
2018-01-23 23:15:21 +0100

    solved student CRUD api task

--- mongodb/main.js
+++ mongodb/main.js
@@ -1,7 +1,18 @@
 var mongoose = require('mongoose');
 var Student = require('./studentmodel');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser());
 
 mongoose.connect('mongodb://localhost/test');
+
+app.get('/', function(req, res) {
+	res.json({
+		info: 'student crud api'
+	})
+});
 
 // task:
 // Create a CRUD api for creating, updating, deleting and reading student info.
@@ -12,45 +23,74 @@ mongoose.connect('mongodb://localhost/test');
 //
 // PUT, DELETE and GET expect a student ID
 
-// create statement
-var chris = new Student({
-	name: 'chris',
-	age: 30,
-	subjects: ['math', 'sport', 'history'],
-	gender: 'male',
+// add a new student
+app.post('/student', function(req, res) {
+	// here we take req.body for the new student
+	console.log(req.body);
+	var newStudent = new Student(req.body);
+	newStudent.save(function(err) {
+		if(err) {
+			return res.send(err);
+		}
+		
+		console.log('user chris has been saved successfully');
+		return res.send({ newStudent: req.body })
+	});
 });
 
-chris.save(function(err) {
-	if(err) throw err;
+// load student info via id 
+app.get('/student/:id', function(req, res) {
+	// read statement
+	Student.findById(req.params.id, function(err, student) {
+		
+		if(!student)
+			return res.send({err: 'student not found'});	
 
-	console.log('user chris has been saved successfully');
-});
-
-
-// read statement
-Student.findById('5a673e4f5c373a19d4f19ed0', function(err, student) {
-	console.log(student);
+		console.log(student);
+		return res.send(student);
+	});
 });
 
 
 // update statement
-Student.findById('5a673e4f5c373a19d4f19ed0', function(err, student) {
-	student.address = {street: 'Bahnhofstr. 1', postal: '12345', city: 'Berlin'};
+app.put('/student/:id', function(req, res) {
+	Student.findById(req.params.id, function(err, student) {
+		if(!student)
+			return res.send({err: 'student not found'});
 
-	student.save(function(err) {
-		if(err) throw err;
+		// check all params that are set in req.body and attach/overwrite the student object
+		for(attr in req.body) {
+			student[attr] = req.body[attr];
+		}
 
-		console.log('student updated');
+		student.save(function(err) {
+			if(err) {
+				return res.send(err);
+			}
+
+			console.log('student updated');
+			return res.send(student);
+		});
 	});
 });
 
-// delete statement
-Student.findById('5a673e4f5c373a19d4f19ed0', function(err, student) {
-	student.remove(function(err) {
-		if(err) throw err;
+// delete a student
+app.delete('/student/:id', function(req, res) {
+	Student.findById(req.params.id, function(err, student) {
+		if(!student)
+			return res.send({err: 'student not found'});
 
-		console.log('student deleted');
+		student.remove(function(err) {
+			if(err) {
+				return res.send(err);
+			}
+
+			console.log('student deleted');
+			return res.send(student);
+		});
 	});
 });
 
-
+app.listen(3000, function() {
+	console.log('app listening on port 3000');
+});
