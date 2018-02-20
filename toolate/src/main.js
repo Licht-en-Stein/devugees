fdadcc88f0093b0b5dc4825d97d0e1345e9c58dd
Jan Schulz <jan.schulz@cileria.com>
2018-02-20 15:20:06 +0100

    added everything except session

--- toolate/src/main.js
+++ toolate/src/main.js
@@ -7,6 +7,7 @@ import pic from './images/beach.jpg';
 import tpl from './template.html';
 import app from './app.html';
 import login from './login.html';
+import moment from 'moment';
 
 $(() => {
 	console.log('jquery is ready');
@@ -28,6 +29,147 @@ function buildApp() {
 		localStorage.setItem('loggedIn', 0);
 		buildLogin();
 	});
+
+	let now = new Date();
+	let dd = now.getDate();
+	let mm = now.getMonth() + 1;
+
+	let yyyy = now.getFullYear();
+	if(dd < 10) {
+		dd = '0' + dd;
+	}
+	if(mm < 10) {
+		mm = '0' + mm;
+	}
+
+	let strNow = dd + '/' + mm + '/' + yyyy;
+	let strTime = now.getHours() + ':' + (now.getMinutes() < 10 ? '0' : '') + now.getMinutes();
+	
+	$('.container').append(`
+		<form id="form-add" style="margin-top: 100px">
+		<div class="form-row">
+			<div class="col-6">
+			<input type="text" id="input-name" class="form-control" placeholder="Max Mueller">
+			</div>
+			<div class="col">
+			<input type="text" id="input-date" class="form-control" placeholder="${strNow}" value="${strNow}">
+			</div>
+			<div class="col">
+			<input type="text" id="input-time" class="form-control" placeholder="${strTime}" value="${strTime}">
+			</div>
+		</div>
+		<button style="margin-top: 15px" type="submit" class="btn btn-dark">Add Laty</button>	
+		<div class="alert alert-success" style="display: none" role="alert">New Laty Added!</div>
+		</form>		
+	`);
+
+	$('#form-add').submit( (e) => {
+		let newLaty = {
+			name: $('#input-name').val(),
+			date: $('#input-date').val(),
+			time: $('#input-time').val()
+		};
+
+		$.ajax({
+			url: 'http://localhost:3000/toolatecomers',
+			method: 'POST',
+			contentType: 'application/json',
+			dataType: 'json',
+			data: JSON.stringify(newLaty)
+		})
+		.done(function(data) {
+			console.log('success', data);
+			
+			let name = $('#input-name').val();
+			let date = $('#input-date').val();
+			let time = $('#input-time').val();						
+
+			let start = moment.duration('09:15', 'HH:mm');
+			let end = moment.duration(data[i].time, 'HH:mm');			
+			let diff = end.subtract(start);
+			let tooLate = diff.hours() + ':' + diff.minutes();
+
+			let row = 
+			`<tr data-id="${data.newTooLateComer._id}">
+				<td>${name}</td>
+				<td>${date}</td>
+				<td>${time} (${tooLate})</td>
+				<td><button type="button" class="btn btn-secondary btn-remove">X</button></td>
+			</tr>`;
+			$('tbody').append(row);		
+
+			$('.alert-success').show().delay(2000).fadeOut('slow');
+			$('#input-name').val('');
+			$('#input-date').val('');
+			$('#input-time').val('');				
+		})
+		.fail(function(xhr) {
+			console.log('error', xhr);
+		});
+	});
+
+	$('.container').append(`
+		<table class="table" style="margin-top: 20px">
+		<thead>
+		<tr>
+			<th scope="col">Name</th>
+			<th scope="col">Date</th>
+			<th scope="col">Time</th>
+			<th scope="col"></th>
+		</tr>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>	
+	`);
+
+	$.ajax({
+		url: 'http://localhost:3000/toolatecomers',
+		method: 'GET',
+		contentType: 'application/json'
+	})
+	.done(function(data) {
+		console.log('success: ' + data);		
+		for(let i=0;i<data.length;i++) {
+
+			let start = moment.duration('09:15', 'HH:mm');
+			let end = moment.duration(data[i].time, 'HH:mm');			
+			let diff = end.subtract(start);
+			let tooLate = diff.hours() + ':' + diff.minutes();
+
+			let row = 
+			`<tr data-id="${data[i]._id}">
+				<td>${data[i].name}</td>
+				<td>${data[i].date}</td>
+				<td>${data[i].time} (${tooLate})</td>
+				<td><button type="button" class="btn btn-secondary btn-remove">X</button></td>			
+			</tr>`;
+			$('tbody').append(row);			
+		}
+	})
+	.fail(function(xhr) {
+		console.log('err: ' + xhr);
+	});
+
+	$('tbody').on('click', '.btn-remove', (e) => {
+		const row = $(e.target).parent().parent();
+		const id = row.attr('data-id');
+		console.log('huhu');
+
+		$.ajax({
+			url: 'http://localhost:3000/toolatecomers/' + id,
+			method: 'DELETE',
+			contentType: 'application/json'
+		})
+		.done(function(data) {
+			console.log('success: ' + data);
+			row.remove();
+		})
+		.fail(function(xhr) {
+			console.log('error: ' + xhr);
+		});		
+	});
+
 }
 
 function buildLogin() {
--- toolate/src/server/express.js
+++ toolate/src/server/express.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose');
 var TooLateComers = require('./toolatecomermodel');
 
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const server = express();
+const bodyParser = require('body-parser');
+
+server.use(bodyParser());
 
 mongoose.connect('mongodb://localhost/latecomer');
 
@@ -28,14 +33,19 @@ server.use(staticMiddleware);
 
 server.post('/toolatecomers', function(req, res) {
 	console.log(req.body);
+
+	if(!req.body.name || !req.body.date || !req.body.time) {
+		return res.send({err: 'name, date and time required'});
+	}
+
 	var newTooLateComer = new TooLateComers(req.body);
-	newTooLateComer.save(function(err) {
+	newTooLateComer.save(function(err, t) {
 		if(err) {
-			return res.send(err);
+			return res.send({err: err});
 		}
 		
 		console.log('new laty has been saved successfully');
-		return res.send({ newTooLateComer: req.body })
+		return res.send({ newTooLateComer: t })
 	});
 });
 
